b2169223e3676e8ca4ae2ffa716f6064a31020e5
Assani Saidi <64076758+assani-saidi@users.noreply.github.com>
2022-05-26 12:41:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // Database and Express configurations variables
 const MONGOOSE_URL = "mongodb://localhost:27017/findhouse";
-const PORT = 4000;
+const PORT = process.env.PORT;
 const HOUSES_IMAGES_URL = "http://localhost:4000/images";
 
 module.exports = {
